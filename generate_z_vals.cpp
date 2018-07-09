997e2c564f3f5765115c60563743c4d2c5be3c2e
birajp <bparikh@u.northwestern.edu>
2018-07-09 17:25:50 +0200

    Moved generator to own function

--- generate_z_vals.cpp
+++ generate_z_vals.cpp
@@ -1,24 +1,28 @@
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <random>
-#include <cmath>
-#include <vector>
+#include "generate_z_vals.h"
 
 int main() {
+    std::vector<double> list = list_of_z(0.01, 30);
+    
+    for(auto x:list)
+        std::cout << x << '\n';
+
+    return 0;
+}
+
+std::vector<double> list_of_z(double variance, int num_of_z) {
     std::random_device rd;
     std::mt19937 gen(rd());
 
     std::default_random_engine generator;
-    std::normal_distribution<> d(0.0, 0.1);
+    std::normal_distribution<> d(0.0, variance);    // mean and variance
 
-    std::vector<double> list_of_z;
+    std::vector<double> list;
 
     for(int n = 0; n < 10; n++) {
         double num = d(gen);
-        list_of_z.push_back(num);
+        list.push_back(num);
         std::cout << num << '\n';
     }
 
-    std::cout << "complete" << '\n';
+    return list;
 }
---
+++ generate_z_vals.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <random>
+#include <cmath>
+#include <vector>
+
+std::vector<double> list_of_z(double variance, int num_of_z);
