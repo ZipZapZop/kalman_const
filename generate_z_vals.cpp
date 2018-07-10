ee7f2ed7479b19d851dbb324452b4c078a85bb01
ZipZapZop <40410831+ZipZapZop@users.noreply.github.com>
2018-07-10 19:06:07 +0200

    Removed comments

--- generate_z_vals.cpp
+++ generate_z_vals.cpp
@@ -1,14 +1,5 @@
 #include "generate_z_vals.h"
 
-// int main() {
-//     std::vector<double> list = list_of_z(0.01, 30);
-    
-//     for(auto x:list)
-//         std::cout << x << '\n';
-
-//     return 0;
-// }
-
 std::vector<double> list_of_z(double variance, double true_val, int num_of_z) {
     std::random_device rd;
     std::mt19937 gen(rd());
