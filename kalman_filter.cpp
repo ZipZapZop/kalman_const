eaf53c2f7bba3a51140424b2c9e6b14e243d0259
birajp <bparikh@u.northwestern.edu>
2018-07-10 12:54:26 +0200

    write to csv

---
+++ kalman_filter.cpp
@@ -0,0 +1,46 @@
+#include "generate_z_vals.h"
+#include "kalman_data.h"
+#include <fstream>
+
+std::vector<double> kalman_filter(double q, double r, double true_val, int num_of_trials);
+
+// constant to estimate == 1.52
+int main()
+{
+    std::vector<double> data = kalman_filter(0.001, 0.01, 1.52, 100);
+    std::ofstream data_out;
+    data_out.open("kalman_data.csv");
+    for(int i = 0; i < data.size(); i++)
+    //     std::cout << list[i] << '\n';
+        data_out << data[i] << '\n';
+    data_out.close();
+}
+
+std::vector<double> kalman_filter(double q, double r, double true_val, int num_of_trials)
+{
+    // Q, R, and sensor measurements
+    kalman_data data = kalman_data(q, r, true_val, num_of_trials);
+    double K;   // gain
+
+    double x_prev = data.x_init;
+    double P_prev = data.P_init;
+    std::vector<double> predicted_data = std::vector<double>();
+    for(int i = 0; i < data.num_of_trials; i++) 
+    {
+        // predict equations
+        double x_apriori = x_prev;
+        double P_apriori = P_prev + data.Q;
+
+        // measurement/correction equations
+        K = P_apriori/(P_apriori + data.R);
+        double x_aposteriori = x_apriori + K*(data.z_values[i] - x_apriori);
+        double P_aposteriori = (1 - K)*P_apriori;
+
+        predicted_data.push_back(x_aposteriori);
+        // std::cout << x_aposteriori << '\n';
+        x_prev = x_aposteriori;
+        P_prev = P_aposteriori;
+    }
+
+    return predicted_data;
+}
