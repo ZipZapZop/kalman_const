da360c32f9039f868d2b64d4f90c1d298eed5228
birajp <bparikh@u.northwestern.edu>
2018-07-10 18:31:45 +0200

    true_val is the mean of the distribution

--- generate_z_vals.cpp
+++ generate_z_vals.cpp
@@ -14,12 +14,12 @@ std::vector<double> list_of_z(double variance, double true_val, int num_of_z) {
     std::mt19937 gen(rd());
 
     std::default_random_engine generator;
-    std::normal_distribution<> d(0.0, variance);    // mean and variance
+    std::normal_distribution<> d(true_val, variance); // KF is trying to estimate true_val
 
     std::vector<double> list;
 
     for(int n = 0; n < num_of_z; n++) {
-        double num = d(gen) + true_val;
+        double num = d(gen);
         list.push_back(num);
     }
 
