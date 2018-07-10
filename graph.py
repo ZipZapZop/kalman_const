c96084bb1e6429335ef76eb094090252adf37c9a
birajp <bparikh@u.northwestern.edu>
2018-07-10 17:58:08 +0200

    init

---
+++ graph.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import csv
+import itertools
+
+def print_graph():
+    with open('kalman_data.csv','r') as d:
+        # reader = csv.reader(d)
+        # data = list(reader)
+        lines = [line.rstrip('\n') for line in open('kalman_data.csv')]
+        lines = list(map(float, lines))
+            
+    plt.figure()
+    plt.plot(lines,'k-',label='Kalman filter outputs')
+    plt.axhline(1.52,color = 'r', label='true value')
+    plt.legend()
+    plt.title('Kalman filter vs the true value (x = 1.52)')
+    plt.show()
+
+print_graph()
