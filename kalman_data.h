6bcb9d2f3c9e83351d2eb1ba16825c49d40e64a7
birajp <bparikh@u.northwestern.edu>
2018-07-10 18:13:23 +0200

    removed x_init and P_init from class

--- kalman_data.h
+++ kalman_data.h
@@ -6,8 +6,6 @@ public:
     std::vector<double> z_values;
     double Q;
     double R;
-    double x_init;
-    double P_init;
     int num_of_trials;
     kalman_data(double q, double r, double true_val, int n);
 };
